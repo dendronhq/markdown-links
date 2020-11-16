59e853a4d377b035c94e5dddc240e1c544786a62
kevinslin <kevin@thence.io>
2020-11-16 09:34:35 -0800

    chore: snap

--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -4,7 +4,5 @@ VERSION_ENGINE_SERVER=0.16.0
 
 yarn unlink @dendronhq/common-all
 yarn unlink @dendronhq/engine-server
-yarn add --force  @dendronhq/common-all
-yarn add --force  @dendronhq/engine-server
-yarn add --force @dendronhq/mume@$VERSION_COMMON_ALL
-yarn add --force @dendronhq/engine-server@$VERSION_ENGINE_SERVER
+yarn add --force  @dendronhq/common-all@$VERSION_ENGINE_SERVER
+yarn add --force  @dendronhq/engine-server@$VERSION_COMMON_ALL
