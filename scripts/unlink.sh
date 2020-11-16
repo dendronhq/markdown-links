4289559612b94fe92a00384885d66434daca8237
kevinslin <kevin@thence.io>
2020-11-16 09:44:13 -0800

    chore: update deps

--- scripts/unlink.sh
+++ scripts/unlink.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 VERSION_COMMON_ALL=0.16.0
-VERSION_ENGINE_SERVER=0.16.0
+VERSION_ENGINE_SERVER=0.16.1-alpha.0
 
 yarn unlink @dendronhq/common-all
 yarn unlink @dendronhq/engine-server
-yarn add --force  @dendronhq/common-all@$VERSION_ENGINE_SERVER
-yarn add --force  @dendronhq/engine-server@$VERSION_COMMON_ALL
+
+yarn add --force  @dendronhq/common-all@$VERSION_COMMON_ALL
+yarn add --force  @dendronhq/engine-server@$VERSION_ENGINE_SERVER
